2c1889949c4cd71c50786bfa13767a5836e94d70
chenz <crazychenz@gmail.com>
2026-04-14 19:07:32 +0100

    Removed cruft, renamed some stuff. Added aliases to each of the config strings.

--- src/contents/code/main.js
+++ src/contents/code/main.js
@@ -1,44 +1,99 @@
-// Modes:
-const DOCKED_DUAL = '[{"h":1600,"w":2560,"x":0,"y":210},{"h":2160,"w":3840,"x":2560,"y":0}]'; //DP-4+eDP-2
-const DOCKED_LG =   '[{"h":1600,"w":2560,"x":0,"y":0},{"h":2160,"w":3840,"x":2560,"y":0}]';
-const MOBILE_ONLY = '[{"h":1600,"w":2560,"x":0,"y":0}]'; //eDP-2
-const DOCKED_SINGLE = '[{"h":2160,"w":3840,"x":2048,"y":0}]'; //DP-4 (!BUG: The x _should_ be zero.)
 
-const TRANSITION_GROUPS = [
-  { // IDE
-    [DOCKED_DUAL]: `{"c":${DOCKED_DUAL},"d":[0,1],"h":2100,"w":1908,"x":2568,"y":8}`, //GOOD
-    [DOCKED_LG]:     `{"c":${DOCKED_LG},"d":[0,1],"h":2100,"w":1908,"x":2568,"y":8}`, //GOOD
-    [MOBILE_ONLY]: `{"c":${MOBILE_ONLY},"d":[0,1],"h":1540,"w":1702,"x":8,"y":8}`, //GOOD
-  },
-  { // Dev Terminal
-    [DOCKED_DUAL]: `{"c":${DOCKED_DUAL},"d":[0],"h":2100,"w":950,"x":4484,"y":8}`, //GOOD
-    [DOCKED_LG]:     `{"c":${DOCKED_LG},"d":[0],"h":2100,"w":950,"x":4484,"y":8}`, //GOOD
-    [MOBILE_ONLY]: `{"c":${MOBILE_ONLY},"d":[0],"h":1540,"w":834,"x":1718,"y":8}`, //GOOD
-  },
-  { // Dev Browser
-    [DOCKED_DUAL]: `{"c":${DOCKED_DUAL},"d":[0],"h":1046,"w":950,"x":5442,"y":8}`, //GOOD
-    [DOCKED_LG]:     `{"c":${DOCKED_LG},"d":[0],"h":1046,"w":950,"x":5442,"y":8}`, //GOOD
-    [MOBILE_ONLY]: `{"c":${MOBILE_ONLY},"d":[1],"h":1540,"w":834,"x":1718,"y":8}`, //GOOD
-  },
-];
+function loadConfiguration() {
+  var CFG = {
+    "Home_Docked": `[{"h":1600,"w":2560,"x":0,"y":210},{"h":2160,"w":3840,"x":2560,"y":0}]`,
+    "Work_Docked": `[{"h":1600,"w":2560,"x":0,"y":0},{"h":2160,"w":3840,"x":2560,"y":0}]`,
+    "Mobile_Only": `[{"h":1600,"w":2560,"x":0,"y":0}]`,
+  }
 
+  // IDE
+  CFG["Home_Docked_IDE"] = `{"c":${CFG["Home_Docked"]},"d":[0,1],"h":2100,"w":1908,"x":2568,"y":8}`;
+  CFG["Work_Docked_IDE"] = `{"c":${CFG["Work_Docked"]},"d":[0,1],"h":2100,"w":1908,"x":2568,"y":8}`;
+  CFG["Mobile_Only_IDE"] = `{"c":${CFG["Mobile_Only"]},"d":[0,1],"h":1540,"w":1702,"x":8,"y":8}`;
+    
+  // Dev Terminal
+  CFG["Home_Docked_DevTerminal"] = `{"c":${CFG["Home_Docked"]},"d":[0],"h":2100,"w":950,"x":4484,"y":8}`;
+  CFG["Work_Docked_DevTerminal"] = `{"c":${CFG["Work_Docked"]},"d":[0],"h":2100,"w":950,"x":4484,"y":8}`;
+  CFG["Mobile_Only_DevTerminal"] = `{"c":${CFG["Mobile_Only"]},"d":[0],"h":1540,"w":834,"x":1718,"y":8}`;
+    
+  // Dev Browser
+  CFG["Home_Docked_DevBrowser"] = `{"c":${CFG["Home_Docked"]},"d":[0],"h":1046,"w":950,"x":5442,"y":8}`;
+  CFG["Work_Docked_DevBrowser"] = `{"c":${CFG["Work_Docked"]},"d":[0],"h":1046,"w":950,"x":5442,"y":8}`;
+  CFG["Mobile_Only_DevBrowser"] = `{"c":${CFG["Mobile_Only"]},"d":[1],"h":1540,"w":834,"x":1718,"y":8}`;
+    
+  // Research Browser
+  CFG["Home_Docked_RnDBrowser"] = `{"c":${CFG["Home_Docked"]},"d":[2],"h":2100,"w":1908,"x":3526,"y":8}`;
+  CFG["Work_Docked_RnDBrowser"] = `{"c":${CFG["Work_Docked"]},"d":[2],"h":2100,"w":1908,"x":3526,"y":8}`;
+  CFG["Mobile_Only_RnDBrowser"] = `{"c":${CFG["Mobile_Only"]},"d":[2],"h":1540,"w":1702,"x":850,"y":8}`;
+    
+  // Research Terminal
+  CFG["Home_Docked_RnDTerminal"] = `{"c":${CFG["Home_Docked"]}:"d":[2],"h":2100,"w":950,"x":2568,"y":8}`;
+  CFG["Work_Docked_RnDTerminal"] = `{"c":${CFG["Work_Docked"]}:"d":[2],"h":2100,"w":950,"x":2568,"y":8}`;
+  CFG["Mobile_Only_RnDTerminal"] = `{"c":${CFG["Mobile_Only"]},"d":[2],"h":1540,"w":834,"x":8,"y":8}`;
+    
+  // Spotify
+  CFG["Home_Docked_Music"] = `{"c":${CFG["Home_Docked"]},"d":[0,1,2,3],"h":766,"w":1268,"x":8,"y":8}`;
+  CFG["Work_Docked_Music"] = `{"c":${CFG["Work_Docked"]},"d":[0,1,2,3],"h":766,"w":1268,"x":8,"y":8}`;
+  CFG["Mobile_Only_Music"] = `{"c":${CFG["Mobile_Only"]},"d":[3],"h":766,"w":1268,"x":8,"y":8}`;
 
-var lastScreensChanged = 0;
-const SCREENS_CHANGED_SETTLE_TIMEOUT = 5000;
-
-// Cache the flat_key of all windows.
-const WINDOW_CACHE = {}
-
-
-// All of the windows currently mapped to each active group member.
-const TRANSITION_RLUT = {};
-// Build TRANSITION_GROUPS reverse lookup
-TRANSITION_GROUPS.forEach(function(group) {
-  Object.keys(group).forEach(function(mode) {
-    TRANSITION_RLUT[group[mode]] = group;
-    console.log(`[zonemech] TRANSITION_RLUT key: ${group[mode]}`);
+  // Generate reverse lookup for configurations.
+  var RCFG = {}
+  Object.keys(CFG).forEach((key) => {
+    rkey = CFG[key];
+    if (!(rkey in RCFG)) {
+      RCFG[rkey] = [];
+    }
+    if (!(key in RCFG[rkey])) {
+      RCFG[rkey].push(key);
+    }
   });
-});
+
+  var GROUPS = [
+    { // IDE
+      [CFG["Home_Docked"]]: CFG["Home_Docked_IDE"],
+      [CFG["Work_Docked"]]: CFG["Work_Docked_IDE"],
+      [CFG["Mobile_Only"]]: CFG["Mobile_Only_IDE"],
+    },
+    { // Dev Terminal
+      [CFG["Home_Docked"]]: CFG["Home_Docked_DevTerminal"],
+      [CFG["Work_Docked"]]: CFG["Work_Docked_DevTerminal"],
+      [CFG["Mobile_Only"]]: CFG["Mobile_Only_DevTerminal"],
+    },
+    { // Dev Browser
+      [CFG["Home_Docked"]]: CFG["Home_Docked_DevBrowser"],
+      [CFG["Work_Docked"]]: CFG["Work_Docked_DevBrowser"],
+      [CFG["Mobile_Only"]]: CFG["Mobile_Only_DevBrowser"],
+    },
+    { // Research Browser
+      [CFG["Home_Docked"]]: CFG["Home_Docked_RnDBrowser"],
+      [CFG["Work_Docked"]]: CFG["Work_Docked_RnDBrowser"],
+      [CFG["Mobile_Only"]]: CFG["Mobile_Only_RnDBrowser"],
+    },
+    { // Research Terminal
+      [CFG["Home_Docked"]]: CFG["Home_Docked_RnDTerminal"],
+      [CFG["Work_Docked"]]: CFG["Work_Docked_RnDTerminal"],
+      [CFG["Mobile_Only"]]: CFG["Mobile_Only_RnDTerminal"],
+    },
+    { // Spotify
+      [CFG["Home_Docked"]]: CFG["Home_Docked_Music"],
+      [CFG["Work_Docked"]]: CFG["Work_Docked_Music"],
+      [CFG["Mobile_Only"]]: CFG["Mobile_Only_Music"],
+    },
+  ];
+
+  // Generate reverse lookup for groups.
+  var RGROUPS = {};
+  // Build GROUPS reverse lookup
+  GROUPS.forEach(function(group) {
+    Object.keys(group).forEach(function(mode) {
+      RGROUPS[group[mode]] = group;
+      console.log(`[zonemech] RGROUPS key: ${group[mode]}`);
+    });
+  });
+
+  return { GROUPS, RGROUPS, CFG, RCFG };
+}
+const { GROUPS, RGROUPS, CFG, RCFG } = loadConfiguration();
 
 
 // Desktop index lookup by ID.
@@ -60,6 +115,7 @@ function getCurrentMode() {
 var CURRENT_MODE = getCurrentMode();
 
 
+//  Get workspace desktop indicies for windows' desktops as array.
 function getDesktops(window) {
   var indicies = window.desktops.map(desktop => DESKTOP_IDX_BY_ID.get(desktop.id))
   indicies.sort();
@@ -67,6 +123,7 @@ function getDesktops(window) {
 }
 
 
+// Get a canonicalized representation of window position/size/desktops.
 function getWindowFlatKey(window) {
   var g = window.frameGeometry;
   var gJson = '"h":' + g.height + ',"w":' + g.width + ',"x":' + g.x + ',"y":' + g.y;
@@ -77,6 +134,7 @@ function getWindowFlatKey(window) {
 }
 
 
+// Return the entry put in the WINDOW_CACHE.
 function windowEntry(window) {
   return { window: window, flat_key: getWindowFlatKey(window) };
 }
@@ -119,6 +177,8 @@ var frameGeometryChangedTimer = null;
 var desktopsChangedTimer = null;
 var screensChangedTimer = null;
 
+// Cache the flat_key of all windows.
+const WINDOW_CACHE = {}
 
 function trackWindow(window) {
     // if (window.internalId != '{9d9f551d-4049-4abe-81e7-cf1d710a2fca}') {
@@ -176,79 +236,84 @@ function untrackWindow(window) {
 }
 
 
-workspace.screensChanged.connect(function() {
-    // Cancel the previous timer if it exists
-    if (screensChangedTimer !== null) {
-      screensChangedTimer.stop();
-      screensChangedTimer = null;
+function handleOutputsUpdate() {
+  screensChangedTimer = null;
+
+  CURRENT_MODE = getCurrentMode();
+
+  // console.log('[zonemech] vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv');
+  // console.log(`[zonemech] New Current Mode: ${CURRENT_MODE}`);
+  // console.log('[zonemech] ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
+
+  Object.keys(WINDOW_CACHE).forEach(function(key) {
+    var value = WINDOW_CACHE[key];
+    //console.log(`[zonemech] ${value.window.internalId} Checking: ${value.flat_key}`)
+
+    // Note: By the time we're here, Plasma has already !@#$ing moved the window.
+
+    if (value.flat_key in RGROUPS) {
+      var window = value.window;
+      group = RGROUPS[value.flat_key];
+      obj = JSON.parse(group[CURRENT_MODE]);
+
+      //console.log(`[zonemech] ${window.internalId} From: ${value.flat_key}`);
+      //console.log(`[zonemech] ${window.internalId} New Position: ${group[CURRENT_MODE]}`);
+
+      newGeometry = { "x": obj.x, "y": obj.y, "width": obj.w, "height": obj.h };
+      window.frameGeometry = newGeometry;
+      window.desktops = obj.d.map(idx => workspace.desktops[idx]);
+
+      //WINDOW_CACHE[key].flat_key = group[CURRENT_MODE];
+
+      //console.log(`[zonemech] ${window.internalId} To: ${group[CURRENT_MODE]}`);
+      //console.log(`[zonemech] ENDENDENDENDENDEND`);
     }
-    if (frameGeometryChangedTimer !== null) {
-      frameGeometryChangedTimer.stop();
-      frameGeometryChangedTimer = null;
-    }
-    if (desktopsChangedTimer !== null) {
-      desktopsChangedTimer.stop();
-      desktopsChangedTimer = null;
-    }
 
-    // Create a new QTimer
-    screensChangedTimer = new QTimer();
-    screensChangedTimer.interval = 500; // 500ms debounce window
-    screensChangedTimer.singleShot = true;
-    screensChangedTimer.timeout.connect(function() {
-      screensChangedTimer = null;
-
-      lastScreensChanged = Date.now();
-      CURRENT_MODE = getCurrentMode();
-
-      // console.log('[zonemech] vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv');
-      // console.log(`[zonemech] New Current Mode: ${CURRENT_MODE}`);
-      // console.log('[zonemech] ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^');
-
-      Object.keys(WINDOW_CACHE).forEach(function(key) {
-        var value = WINDOW_CACHE[key];
-        //console.log(`[zonemech] ${value.window.internalId} Checking: ${value.flat_key}`)
-
-        // ! By the time we're here, Plasma has already !@#$ing moved the window.
-
-        if (value.flat_key in TRANSITION_RLUT) {
-          var window = value.window;
-          group = TRANSITION_RLUT[value.flat_key];
-          obj = JSON.parse(group[CURRENT_MODE]);
-
-          //console.log(`[zonemech] ${window.internalId} From: ${value.flat_key}`);
-          //console.log(`[zonemech] ${window.internalId} New Position: ${group[CURRENT_MODE]}`);
-
-          newGeometry = { "x": obj.x, "y": obj.y, "width": obj.w, "height": obj.h };
-          window.frameGeometry = newGeometry;
-          window.desktops = obj.d.map(idx => workspace.desktops[idx]);
-
-          //WINDOW_CACHE[key].flat_key = group[CURRENT_MODE];
-
-          //console.log(`[zonemech] ${window.internalId} To: ${group[CURRENT_MODE]}`);
-          //console.log(`[zonemech] ENDENDENDENDENDEND`);
-        }
-
-      });
-
-    });
-    screensChangedTimer.start();
-});
+  });
+}
 
 
-// Track all window geometries
-workspace.windowAdded.connect(trackWindow);
-workspace.windowRemoved.connect(untrackWindow);
-workspace.windowList().forEach(trackWindow);
-lastScreensChanged = Date.now();
+function handleOutputsUpdateTiming() {
+  // Cancel the previous timer if it exists
+  if (screensChangedTimer !== null) {
+    screensChangedTimer.stop();
+    screensChangedTimer = null;
+  }
+  if (frameGeometryChangedTimer !== null) {
+    frameGeometryChangedTimer.stop();
+    frameGeometryChangedTimer = null;
+  }
+  if (desktopsChangedTimer !== null) {
+    desktopsChangedTimer.stop();
+    desktopsChangedTimer = null;
+  }
+
+  // Create a new QTimer
+  screensChangedTimer = new QTimer();
+  screensChangedTimer.interval = 500; // 500ms debounce window
+  screensChangedTimer.singleShot = true;
+  screensChangedTimer.timeout.connect(handleOutputsUpdate);
+  screensChangedTimer.start();
+}
 
 
-console.log('[zonemech] ');
-console.log('[zonemech] ');
-console.log('[zonemech] ');
-console.log(`[zonemech] Script loaded. Monitoring screen changes. ${Date.now()}`);
-console.log('[zonemech] ');
-console.log('[zonemech] ');
-console.log('[zonemech] ');
+function init() {
+  workspace.screensChanged.connect(handleOutputsUpdateTiming);
 
+  // Track all window geometries
+  workspace.windowAdded.connect(trackWindow);
+  workspace.windowRemoved.connect(untrackWindow);
+  workspace.windowList().forEach(trackWindow);
 
+  // Add a noticeable log entry for script restart.
+  console.log('[zonemech] ');
+  console.log('[zonemech] ');
+  console.log('[zonemech] ');
+  console.log(`[zonemech] Script loaded. Monitoring screen changes. ${Date.now()}`);
+  console.log('[zonemech] ');
+  console.log('[zonemech] ');
+  console.log('[zonemech] ');
+
+}
+
+init();
